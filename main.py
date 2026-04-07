a669c7dd8611c569b3a41c2c4d1beb9a4de1dde4
nazir-karimi <zamirkarimi68@gmail.com>
2026-04-07 08:59:59 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("8796614951:AAEXi-WiaaTDHgEOvNIjQlxubununrGdJ-M")
+TOKEN = os.getenv("8796614951:AAEXi-WiaaTDHgEOvNIjQlxubununrGdJ-M")  # 👈 این خط مهمه
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام 👋 ربات روشن شد!")
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("پیام شما: " + update.message.text)
+    await update.message.reply_text("ربات روشنه ✅")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-print("Bot started...")
 
 app.run_polling()
-print("Bot started on server...")
-print("update")   # این خطو اضافه کن
